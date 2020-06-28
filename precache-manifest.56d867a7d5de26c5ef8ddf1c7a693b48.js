6cb42e8a7ef6d6922b578d163ecf40d4b5a129b7
ComputerScienceHayk <59318612+ComputerScienceHayk@users.noreply.github.com>
2020-06-29 00:37:32 +0400

    Updates

---
+++ precache-manifest.56d867a7d5de26c5ef8ddf1c7a693b48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa6bd3e2550e13936305fcc0cdd0388f",
+    "url": "/react-admin-sidebar/index.html"
+  },
+  {
+    "revision": "f60c178e031390e6e616",
+    "url": "/react-admin-sidebar/static/css/main.65007c51.chunk.css"
+  },
+  {
+    "revision": "df898d0b9d97f1d418d4",
+    "url": "/react-admin-sidebar/static/js/2.b60f63e9.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/react-admin-sidebar/static/js/2.b60f63e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f60c178e031390e6e616",
+    "url": "/react-admin-sidebar/static/js/main.e98c325f.chunk.js"
+  },
+  {
+    "revision": "e859e3e40de247e4ad11",
+    "url": "/react-admin-sidebar/static/js/runtime-main.757a23e4.js"
+  }
+]);
